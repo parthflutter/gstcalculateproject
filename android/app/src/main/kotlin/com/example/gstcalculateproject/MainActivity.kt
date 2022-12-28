76d0fb9169a1719a40a942562ca32aead08b0804
user <savaliyaparth712@gmail.com>
2022-12-28 10:19:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gstcalculateproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gstcalculateproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'Homepage.dart';
+
+
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
+  );
+}
